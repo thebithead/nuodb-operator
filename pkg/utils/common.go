2c125527a0c9e2f20abdf9cbc5dd085d32ae48ad
Tom Gates <tgates@nuodb.com>
2019-11-05 18:38:23 -0500

    Set revision to v2.0.1

--- pkg/utils/common.go
+++ pkg/utils/common.go
@@ -55,7 +55,7 @@ import (
 )
 
 const (
-	NuodbOperatorVersion = "v2.0.0"
+	NuodbOperatorVersion = "v2.0.1"
 	// Default path to the Operator "etc" directory when
 	// running the Operator "inside" of a K8s cluster.
 	DefaultOperatorEtcDir = "/usr/local/etc/nuodb-operator"
